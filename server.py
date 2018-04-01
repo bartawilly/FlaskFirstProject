26c594b273793827fd17ff24ab3d3e581c3b30f0
bartawilly <ahmed_nasser269@yahoo.com>
2018-04-01 17:06:45 +0200

    finish till : cookies

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template,make_response,session
 app = Flask(__name__)
-
+app.secret_key = 'sfkdngkshfgjlshfgjkshg'
 @app.route('/')
 def hello_world():
    return 'hello word'
@@ -45,14 +45,64 @@ def hello_name(user):
 def score(score):
    return render_template('score.html', marks = score)
 #######
-@app.route('/result')
+@app.route('/result',methods = ['POST', 'GET'])
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+   if request.method=='POST':
+       result=request.form
+   return render_template('result.html', result = result) 
+#######
+@app.route("/degree")
+def degree():
+    return render_template("degree.html")
 #######
 @app.route("/js")
 def js():
    return render_template("js.html")
+#######
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+   if request.method == 'POST':
+       user = request.form['nm']
+       resp = make_response(render_template('readcookie.html'))
+       resp.set_cookie('userID', user)
+   return resp
+########  
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome '+name+'</h1>'
+########
+@app.route('/cookies')
+def cookies():
+   return render_template('cookies.html')
+########
+@app.route('/defualt')
+def defualt():
+   if 'username' in session:
+      username = session['username']
+      return 'Logged in as ' + username + '<br>' + \
+      "<b><a href = '/logout2'>click here to log out</a></b>"
+   return "You are not logged in <br><a href = '/login2'></b>" + \
+      "click here to log in</b></a>"
+########
+@app.route('/login2', methods = ['GET', 'POST'])
+def login2():
+   if request.method == 'POST':
+      session['username'] = request.form['username']
+      return redirect(url_for('defualt'))
+   return '''
+   <form action = "/login2" method = "POST">
+      <p><input type = text name = username/></p>
+      <p><input type = submit value = login></p>
+   </form>
+   '''
+#######
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return redirect(url_for('defualt'))
+#######
 if __name__ == '__main__':
     app.debug = True
     app.run()
